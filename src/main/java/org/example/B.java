8d900aa7238a635caf6081a289027fe9f19fe3bd
hejiandong <hejiandong@atplatform.net>
2024-06-07 11:08:22 +0800

    B commit

--- src/main/java/org/example/B.java
+++ src/main/java/org/example/B.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class B {
     public static void main(String[] args) {
-        System.out.println("second commit");
+        System.out.println("B commit");
     }
 }
